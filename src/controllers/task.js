6ff09c0637d0a96332ba494cb5986b565dd7f202
carlosGabrielCarreno <carrenocarlosgabriel@gmail.com>
2023-01-24 17:33:23 -0300

    starting work with controllers from Task

---
+++ src/controllers/task.js
@@ -0,0 +1,20 @@
+const { response, request } = require('express');
+const { Task } = require('../db');
+
+const getTasks = () => {};
+
+const createTask = async (req = request, res = response) => {
+	const { description } = req.body;
+
+	if (description.length <= 1) return res.status(404).send('Missing Data');
+
+	try {
+		//const task = await Task.cre
+	} catch (error) {
+		res.status(400).send(error);
+	}
+};
+
+module.exports = {
+	createTask,
+};
